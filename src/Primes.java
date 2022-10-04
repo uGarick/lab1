290b37fa091cec94f2cfba8282f348a449c03ab6
Igor <igor.gaz2004@gmail.com>
2022-10-04 16:13:27 +0300

    Some changes in Primes.java

--- src/Primes.java
+++ src/Primes.java
@@ -2,8 +2,7 @@ public class Primes {
     public static void main(String[] args) {
         for(int i=2; i<=100; i++) {
             if (isPrime(i)) {
-                System.out.print(i);
-                System.out.print(", ");
+                System.out.println(i);
             }
         }
     }
